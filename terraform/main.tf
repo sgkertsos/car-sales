a0da6f6a111117ba39d76e117da6d16d0ee6c0e2
dgkertsos <164078332+dgkertsos@users.noreply.github.com>
2024-03-30 19:01:18 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
 
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  credentials = "<Path to your service account key file>"
+  project = "<Your Project ID>"
+  region  = "<Your Region>"
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "<Your Unique Bucket Name>"
+  location      = "<Your Location>"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
