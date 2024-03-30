ca89267ce18027181a970cbc79c5fe3626beee42
dgkertsos <164078332+dgkertsos@users.noreply.github.com>
2024-03-30 20:25:32 +0200

    Update bl_upload_data_to_gcs_partitioned.py

--- mage/data_exporters/bl_upload_data_to_gcs_partitioned.py
+++ mage/data_exporters/bl_upload_data_to_gcs_partitioned.py
@@ -1,1 +1,30 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
 
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/keys/mage_service_account.json"
+
+bucket_name = '<Your Unique Bucket Name>'
+project_id = '<Your Project ID>'
+
+table_name = 'car_sales_partitioned'
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    data['saledate'] = data['saledate'].dt.date
+
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['saledate'],
+        filesystem=gcs
+    )
